210b5c5b18d4fff7a4002b56d8fbebb2e4aa895e
Stephen Johnston <sdjohnston1990@gmail.com>
2023-01-17 11:26:37 -0400

    update port number

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 require('dotenv').config()
 
-const PORT = process.env.PORT
+const PORT = 4000
 
 const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
